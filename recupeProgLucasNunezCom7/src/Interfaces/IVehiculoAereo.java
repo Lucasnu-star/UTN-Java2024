353caf50881095d4e1c1e84c88ef5019f0aa5668
Lucas Gabriel Nuñez <65827279+Lucasnu-star@users.noreply.github.com>
2024-10-13 21:07:16 -0300

    RecuperatorioPrimerParcial-Java

---
+++ recupeProgLucasNunezCom7/src/Interfaces/IVehiculoAereo.java
@@ -0,0 +1,9 @@
+package Interfaces;
+/**
+ * Interfaz que tiene dos metodos y la implementa la clase Avion y Helicoptero
+ */
+public interface IVehiculoAereo {
+
+    String despegar();
+    String aterrizar();
+}
